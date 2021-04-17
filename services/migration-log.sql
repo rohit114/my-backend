faee1624e40f5619077fc9b55de5c2969ce2f5e3
Rohit Kumar <rohit.kumar@yulu.bike>
2021-04-17 16:34:24 +0530

    added migration log file

---
+++ services/migration-log.sql
@@ -0,0 +1,28 @@
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(50) DEFAULT '0',
+  `last_name` varchar(50) DEFAULT NULL,
+  `phone` varchar(15) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `is_active` int DEFAULT '1',
+  `created_by` int DEFAULT NULL,
+  `created_dt` int DEFAULT NULL,
+  `updated_by` int DEFAULT NULL,
+  `updated_dt` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+
+
+CREATE TABLE `user_asset_map` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_d` int(11) DEFAULT NULL,
+  `asset_id` int(11) DEFAULT 0,
+  `created_by` int DEFAULT NULL,
+  `created_dt` int DEFAULT NULL,
+  `updated_by` int DEFAULT NULL,
+  `updated_dt` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (user_d) REFERENCES users(id)
+);
+
