aac058658c141c874fb62592866de25d0b89f07b
rohitkumar114 <85483100+rohitkumar114@users.noreply.github.com>
2022-09-29 23:36:15 +0530

    added hashmap for CRUD operation

--- routes/index.js
+++ routes/index.js
@@ -17,5 +17,21 @@ router.post('/bulk-insert', function(req, res, next) {
    User.bulkInsertDemo(req, res);
 });
 
+router.get('/get-users', function(req, res, next) {
+   User.getAllDataFromMap(req, res);
+});
+
+router.get('/get-users-by-id', function(req, res, next) {
+   User.getDataFromMapById(req, res);
+});
+
+router.post('/create-user-v2', function(req, res, next) {
+   User.createDataInMap(req, res);
+});
+
+router.put('/update-user-v2', function(req, res, next) {
+   User.updateDataFromMapById(req, res);
+});
+
 
 module.exports = router;
